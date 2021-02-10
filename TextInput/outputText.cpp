69f3105ab5091f2fcd64d9c6d23b02c1e2869a70
Marcus <Quesenekcam@gmail.com>
2021-02-09 20:10:07 -0500

    Create outputText.cpp

---
+++ TextInput/outputText.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    string input = "";
+
+    cout << "Enter Something\n";
+    cin >> input;
+    cout << (input + " your input");
+
+    return 0;
+}
